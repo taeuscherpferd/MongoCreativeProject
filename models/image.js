af529c260a71a71d1c3542d61621e976422ebf7a
Kailean O'Keefe <ibber@myokcorral.com>
2018-11-15 00:22:32 +0000

    minor update

--- models/image.js
+++ models/image.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //TODO: This needs to be redone to accept a photo instead of text
 var photoSchema = mongoose.Schema({ //Defines the Schema for this database
-  Name: String,
+  img: {data: Buffer, contentType: String} 
 });
 
 mongoose.model('image', photoSchema);
